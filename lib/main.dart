9c38ae0a8e8d8c462cb06a1d011aac960d299987
Meet Shah <shahmeet2141@gmail.com>
2022-02-17 22:01:55 +0530

    [ADD] crashlytics

--- lib/main.dart
+++ lib/main.dart
@@ -1,21 +1,30 @@
+import 'package:firebase_analytics/firebase_analytics.dart';
 import 'package:firebase_core/firebase_core.dart';
+import 'package:firebase_crashlytics/firebase_crashlytics.dart';
 import 'package:flutter/material.dart';
 
-void main() {
-  Firebase.initializeApp();
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
+  FlutterError.onError = FirebaseCrashlytics.instance.recordFlutterError;
+  await FirebaseCrashlytics.instance.setCrashlyticsCollectionEnabled(true);
   runApp(const MyApp());
 }
+
+// com.example.cicd
 
 class MyApp extends StatelessWidget {
   const MyApp({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
+    FirebaseAnalytics analytics = FirebaseAnalytics.instance;
     return MaterialApp(
       title: 'Flutter CI/CD Git Actions',
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
+      navigatorObservers: [FirebaseAnalyticsObserver(analytics: analytics)],
       home: const MyHomePage(title: 'Flutter Demo Home Page'),
     );
   }
@@ -59,7 +68,18 @@ class _MyHomePageState extends State<MyHomePage> {
         ),
       ),
       floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
+        onPressed: () async {
+          _incrementCounter();
+          //
+          // await FirebaseAnalytics.instance
+          //     .setUserProperty(name: 'age', value: '25');
+          // await FirebaseAnalytics.instance.logEvent(name: 'meet', parameters: {
+          //   'userId': 1234,
+          // });
+          // await FirebaseAnalytics.instance
+          //     .setCurrentScreen(screenName: 'Products');
+          // FirebaseCrashlytics.instance.crash();
+        },
         tooltip: 'Increment',
         child: const Icon(Icons.add),
       ), // This trailing comma makes auto-formatting nicer for build methods.
